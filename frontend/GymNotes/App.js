9cb24a9e8b777a3f5a1424cb1788225abad02392
JakeLDev <me@jakelyell.dev>
2023-04-11 22:27:36 +0800

    react native with expo and nativewind working? real

---
+++ frontend/GymNotes/App.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+import "./styles";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-blue-800 font-extrabold">Universal React with Expo</Text>
+    </View>
+  );
+}
